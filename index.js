6ce0c6dd63da27269726f57188a40e2194f26968
Saibou <saiboub80@gmail.com>
2023-03-25 10:04:54 -0400

    Adding styles to REST-Rant

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const app = express()
 
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
+
 
 app.use('/places', require('./controllers/places'))
 
