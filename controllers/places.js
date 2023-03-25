c7b98b6634759fe96c74f5ed373fc910e4dc9058
Saibou <saiboub80@gmail.com>
2023-03-25 14:14:20 -0400

    Adding Bootstrap grid to index page

--- controllers/places.js
+++ controllers/places.js
@@ -1,19 +1,20 @@
 const router = require('express').Router()
 
 router.get('/', (req, res) => {
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: 'http://placekitten.com/250/250'
-      }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: 'http://placekitten.com/250/250'
-      }]
+  let places = [{
+    name: 'H-Thai-ML',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Thai, Pan-Asian',
+    pic: '../images/h-thai-ml-tables.png'
+  }, {
+      name: 'Coding Cat Cafe',
+      city: 'Phoenix',
+      state: 'AZ',
+      cuisines: 'Coffee, Bakery',
+      pic: '../images/coffee-cat.jpg'
+  }]
+  
       
       res.render('places/index', { places })
 })
--- views/error404.jsx
+++ views/error404.jsx
@@ -6,6 +6,7 @@ function error404 () {
           <main>
               <h1>404: PAGE NOT FOUND</h1>
               <p>Oops, sorry, we can't find this page!</p>
+              <img src='../images/404.png' alt='404 image' />
           </main>
       </Def>
     )
