a252b5c1e98169523ab2dfb5aa4da4360f7c7f4a
Saibou <saiboub80@gmail.com>
2023-04-08 09:51:15 -0400

    add comments and rating

--- controllers/places.js
+++ controllers/places.js
@@ -34,6 +34,28 @@ router.post('/', (req, res) => {
     });
 });
 
+
+router.post('/:id/comment', (req, res) => {
+  console.log(req.body)
+  db.Place.findById(req.params.id)
+  .then(place => {
+      db.Comment.create(req.body)
+      .then(comment => {
+          place.comments.push(comment.id)
+          place.save()
+          .then(() => {
+              res.redirect(`/places/${req.params.id}`)
+          })
+      })
+      .catch(err => {
+          res.render('error404')
+      })
+  })
+  .catch(err => {
+      res.render('error404')
+  })
+})
+
       
     
 router.get('/new', (_req, res) => {
